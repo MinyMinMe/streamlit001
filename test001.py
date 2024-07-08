440a4538d16c055bf9f936249bf2f67b11b6c78d
MinyMin <155429777+MinyMinMe@users.noreply.github.com>
2024-07-09 08:50:17 +0900

    Update test001.py

--- test001.py
+++ test001.py
@@ -5,8 +5,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 
 ## Load Config
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv(), override=True)
+#from dotenv import load_dotenv, find_dotenv
+#load_dotenv(find_dotenv(), override=True)
 
 st.title('🦜🔗 ADLIB ver 1.0')
 #st.set_page_config(page_title="🦜🔗 내 맘대로 생성기 ")
